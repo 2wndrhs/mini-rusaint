39caec1a84242b5ff1baf6ecee1cc2b7ffa23a92
2wndrhs <2wndrhs@gmail.com>
2025-01-06 00:15:48 +0900

    feat: SAP EVENT를 생성하는 빌더 추가

--- src/webdynpro/event.rs
+++ src/webdynpro/event.rs
@@ -1,15 +1,94 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fmt::{Display, Formatter, Result as FmtResult},
+};
 
+use derive_builder::Builder;
 use regex::Regex;
 
-pub fn decode_sap_event_encoding(encoded: &str) -> String {
+const SAP_ENCODED_NEWLINE: &str = "~E001";
+const SAP_ENCODED_OPEN_BRACE: &str = "~E002";
+const SAP_ENCODED_CLOSE_BRACE: &str = "~E003";
+const SAP_ENCODED_COLON: &str = "~E004";
+const SAP_ENCODED_COMMA: &str = "~E005";
+
+#[derive(Debug, Builder)]
+#[builder(setter(into))]
+pub struct SapEvent {
+    event: String,
+    control: String,
+
+    #[builder(setter(each = "add_parameter"))]
+    #[builder(default)]
+    parameters: HashMap<String, String>,
+
+    #[builder(setter(each = "add_ucf_parameter"))]
+    #[builder(default)]
+    ucf_parameters: HashMap<String, String>,
+
+    #[builder(setter(each = "add_custom_parameter"))]
+    #[builder(default)]
+    custom_parameters: HashMap<String, String>,
+}
+
+impl Display for SapEvent {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}_{}", self.event, self.control)?;
+
+        write!(f, "{}", SAP_ENCODED_OPEN_BRACE)?;
+        for (i, (key, value)) in self.parameters.iter().enumerate() {
+            write!(f, "{}{}{}", key, SAP_ENCODED_COLON, encode_sap_event(value))?;
+            if i < self.parameters.len() - 1 {
+                write!(f, "{}", SAP_ENCODED_COMMA)?;
+            }
+        }
+        write!(f, "{}", SAP_ENCODED_CLOSE_BRACE)?;
+
+        write!(f, "{}", SAP_ENCODED_OPEN_BRACE)?;
+        for (i, (key, value)) in self.ucf_parameters.iter().enumerate() {
+            write!(f, "{}{}{}", key, SAP_ENCODED_COLON, encode_sap_event(value))?;
+            if i < self.ucf_parameters.len() - 1 {
+                write!(f, "{}", SAP_ENCODED_COMMA)?;
+            }
+        }
+        write!(f, "{}", SAP_ENCODED_CLOSE_BRACE)?;
+
+        write!(f, "{}", SAP_ENCODED_OPEN_BRACE)?;
+        for (i, (key, value)) in self.custom_parameters.iter().enumerate() {
+            write!(f, "{}{}{}", key, SAP_ENCODED_COLON, encode_sap_event(value))?;
+            if i < self.custom_parameters.len() - 1 {
+                write!(f, "{}", SAP_ENCODED_COMMA)?;
+            }
+        }
+        write!(f, "{}", SAP_ENCODED_CLOSE_BRACE)?;
+
+        Ok(())
+    }
+}
+
+/// 문자열을 sap event가 URL에서 전달되는 방식으로 인코딩합니다.
+pub fn encode_sap_event(input: &str) -> String {
+    let mut encoded = String::new();
+
+    for ch in input.chars() {
+        match ch {
+            'A'..='Z' | 'a'..='z' | '0'..='9' | '-' | '_' | '.' | '~' => encoded.push(ch),
+            _ => encoded.push_str(format!("~{:04X}", ch as u32).as_str()),
+        }
+    }
+
+    encoded
+}
+
+/// sap event encoding된 문자열을 디코딩합니다.
+pub fn decode_sap_event(encoded: &str) -> String {
     // sap event encoding에서 쓰이는 특수 문자 처리
     let mut replacements = HashMap::new();
-    replacements.insert("~E001", "\n");
-    replacements.insert("~E002", "{");
-    replacements.insert("~E003", "}");
-    replacements.insert("~E004", ":");
-    replacements.insert("~E005", ",");
+    replacements.insert(SAP_ENCODED_NEWLINE, "\n");
+    replacements.insert(SAP_ENCODED_OPEN_BRACE, "{");
+    replacements.insert(SAP_ENCODED_CLOSE_BRACE, "}");
+    replacements.insert(SAP_ENCODED_COLON, ":");
+    replacements.insert(SAP_ENCODED_COMMA, ",");
 
     // 특수 문자 변환
     let mut result = encoded.to_string();
@@ -30,7 +109,7 @@ pub fn decode_sap_event_encoding(encoded: &str) -> String {
                     return c.to_string();
                 }
             }
-            // 디코딩이 실패하면 원래 문자열 반환
+            // 변환이 실패하면 원래 문자열 반환
             caps[0].to_string()
         })
         .to_string();
@@ -43,24 +122,48 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_client_inspector_decode() {
-        let input =
-            "ClientInspector_Notify~E002Id~E004WD01~E005Data~E004ClientWidth~003A995px~E003~E002ResponseData~E004delta~E005EnqueueCardinality~E004single~E003~E002~E003";
-        let expected = "ClientInspector_Notify{Id:WD01,Data:ClientWidth:995px}{ResponseData:delta,EnqueueCardinality:single}{}";
-        assert_eq!(decode_sap_event_encoding(input), expected);
-    }
-
-    #[test]
-    fn test_button_press_decode() {
+    fn test_sap_event_decode() {
         let input = "Button_Press~E002Id~E004ZCMW_PERIOD_RE.ID_0DC742680F42DA9747594D1AE51A0C69~003AVIW_MAIN.BUTTON_PREV~E003~E002ResponseData~E004delta~E005ClientAction~E004submit~E003~E002~E003";
         let expected = "Button_Press{Id:ZCMW_PERIOD_RE.ID_0DC742680F42DA9747594D1AE51A0C69:VIW_MAIN.BUTTON_PREV}{ResponseData:delta,ClientAction:submit}{}";
-        assert_eq!(decode_sap_event_encoding(input), expected);
+        assert_eq!(decode_sap_event(input), expected);
     }
 
     #[test]
-    fn test_form_request_decode() {
-        let input = "Form_Request~E002Id~E004sap.client.SsrClient.form~E005Async~E004false~E005FocusInfo~E004~0040~007B~0022sFocussedId~0022~003A~0022BUTTON_ID~0022~007D~E005Hash~E004~E005DomChanged~E004false~E005IsDirty~E004false~E003~E002ResponseData~E004delta~E003~E002~E003";
-        let expected = "Form_Request{Id:sap.client.SsrClient.form,Async:false,FocusInfo:@{\"sFocussedId\":\"BUTTON_ID\"},Hash:,DomChanged:false,IsDirty:false}{ResponseData:delta}{}";
-        assert_eq!(decode_sap_event_encoding(input), expected);
+    fn test_sap_event_builder() {
+        let sap_event = SapEventBuilder::default()
+            .event("ComboBox")
+            .control("Select")
+            .add_parameter((
+                "Id".to_string(),
+                "ZCMW_PERIOD_RE.ID_0DC742680F42DA9747594D1AE51A0C69:VIW_MAIN.PERYR".to_string(),
+            ))
+            .add_parameter(("Key".to_string(), "2024".to_string()))
+            .add_ucf_parameter(("ResponseData".to_string(), "delta".to_string()))
+            .add_ucf_parameter(("ClientAction".to_string(), "submit".to_string()))
+            .build()
+            .unwrap();
+
+        assert_eq!(sap_event.event, "ComboBox");
+        assert_eq!(sap_event.control, "Select");
+        assert_eq!(
+            sap_event.parameters.get("Id").unwrap(),
+            "ZCMW_PERIOD_RE.ID_0DC742680F42DA9747594D1AE51A0C69:VIW_MAIN.PERYR"
+        );
+        assert_eq!(
+            sap_event.ucf_parameters.get("ResponseData").unwrap(),
+            "delta"
+        );
+
+        // Test the Display implementation
+        let sap_event_str = sap_event.to_string();
+
+        assert!(sap_event_str.contains("ComboBox_Select"));
+        assert!(sap_event_str.contains(
+            "Id~E004ZCMW_PERIOD_RE.ID_0DC742680F42DA9747594D1AE51A0C69~003AVIW_MAIN.PERYR"
+        ));
+        assert!(sap_event_str.contains("Key~E0042024"));
+        assert!(sap_event_str.contains("ResponseData~E004delta"));
+        assert!(sap_event_str.contains("ClientAction~E004submit"));
+        assert!(sap_event_str.contains("~E002~E003"));
     }
 }
